543132edafbabc59e5b47cd7f2ef2d4238d2f74d
walisonpalladino <walisonteixeira2011@gmail.com>
2023-01-24 18:27:22 -0300

    Exercício if e else

--- fundamentos/secao-03-indroducao-a-javascript/dia-01-introducao-a-javascript/scripts.js
+++ fundamentos/secao-03-indroducao-a-javascript/dia-01-introducao-a-javascript/scripts.js
@@ -1,15 +1,10 @@
-const myName = "Walison";
-let birthCity = "Betim";
-let birthYear = 1986;
-
-console.log(myName);
-console.log(birthCity);
-console.log(birthYear);
-
-birthYear = 2030;
-
-console.log(birthYear);
-
-birthCity = "Belo Horizonte"
-
-console.log(birthCity);
+const nota = 91;
+if (nota >= 80) {
+    console.log("Parabéns, você faz parte do grupo das pessoas aprovadas!");
+} 
+else if (nota < 80 && nota >= 60) {
+    console.log("Você está na nossa lista de espera");
+} 
+else if (nota < 60) {
+    console.log("Infelizmente, você reprovou.");
+}
