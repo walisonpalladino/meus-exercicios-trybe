6a826cfc9dfc22b081a4c199d9a1f0b1c7474510
walisonpalladino <walisonteixeira2011@gmail.com>
2023-01-25 19:26:36 -0300

    Exercícios finalizados

--- fundamentos/secao-03-indroducao-a-javascript/dia-02-array-e-loop-for/exercicios-agora-e-pratica/script.js
+++ fundamentos/secao-03-indroducao-a-javascript/dia-02-array-e-loop-for/exercicios-agora-e-pratica/script.js
@@ -90,13 +90,13 @@
 // console.log(numeros);
 
 
-// let numeros = [];
+let numeros = [];
 
-// for( let index = 1; index <= 25; index += 1) {
-//     numeros.push(index);
-// }
-// console.log(numeros);
+for( let index = 1; index <= 25; index += 1) {
+    numeros.push(index);
+}
+console.log(numeros);
 
-// for (let index = 0; index < numeros.length; index += 1) {
-//     console.log(numeros[index] / 2);
-//   };
+for (let index = 0; index < numeros.length; index += 1) {
+    console.log(numeros[index] / 2);
+  };
