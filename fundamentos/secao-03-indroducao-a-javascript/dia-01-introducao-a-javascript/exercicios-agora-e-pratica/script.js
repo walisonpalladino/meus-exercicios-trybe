028f77e3758a7ffc50a9e1ff4da3e1c1d909af5d
walisonpalladino <walisonteixeira2011@gmail.com>
2023-01-24 22:31:36 -0300

    Primeiros exercícios

---
+++ fundamentos/secao-03-indroducao-a-javascript/dia-01-introducao-a-javascript/exercicios-agora-e-pratica/script.js
@@ -0,0 +1,45 @@
+// const a = 2;
+// const b = 3;
+ 
+// console.log(a + b);
+// console.log(a - b);
+// console.log(a * b);
+// console.log(a / b);
+// console.log(a % b);
+
+// const primeiro = 1;
+// const segundo = 5;
+
+// // maior de dois números
+
+// if (primeiro > segundo) {
+//     console.log("Primeiro é maior");
+// } else {
+//     console.log("Segundo é maior");
+// }
+
+// const x = 5;
+// const y = 100;
+// const z = 100;
+
+// maior de três numeros
+
+// if (x > y && x > z) {
+//     console.log("X é maior");
+// } else if (y > x && y > z) {
+//     console.log("Y é maior");
+// } else {
+//     console.log("Z é maior");
+// }
+
+const valor = 0;
+
+// retornar positivo, negativo ou zero
+
+if (valor > 0) {
+    console.log("Positivo");
+} else if (valor < 0) {
+    console.log("Negativo");
+} else {
+    console.log("Zero");
+}
