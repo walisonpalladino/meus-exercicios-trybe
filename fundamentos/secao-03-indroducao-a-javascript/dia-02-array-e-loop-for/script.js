c2e1be4cfbdad5a9e49b80084ce9177bdc176743
walisonpalladino <walisonteixeira2011@gmail.com>
2023-01-25 18:16:32 -0300

    Exercício for concluido

--- fundamentos/secao-03-indroducao-a-javascript/dia-02-array-e-loop-for/script.js
+++ fundamentos/secao-03-indroducao-a-javascript/dia-02-array-e-loop-for/script.js
@@ -14,3 +14,22 @@
 
 // console.log(menu);
 
+// let numero = 5;
+
+// for(let contador = 1; contador <= 9; contador ++) {
+//     console.log(numero * contador);
+// }
+
+// let listaDeNomes = ["Joana", "Maria", "Lucas"];
+// for(let indice = 0; indice < listaDeNomes.length; indice++) {
+//     let mensagem = "Boas vindas," + listaDeNomes[indice] + "!";
+//     console.log(mensagem);
+// }
+
+//Utilize o for para imprimir os elementos da lista groceryList com o console.log():
+
+let groceryList = ['Arroz', 'Feijão', 'Alface', 'Melancia'];
+
+for(let index = 0 ; index < groceryList.length; index += 1) {
+    console.log(groceryList[index]);
+}
