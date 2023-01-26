011bbd7ce26dafc5909e651a24963f1d7476ba00
walisonpalladino <walisonteixeira2011@gmail.com>
2023-01-26 19:07:54 -0300

    Exercícios incompletos

--- fundamentos/secao-03-indroducao-a-javascript/dia-04-objetos-e-funcoes/script.js
+++ fundamentos/secao-03-indroducao-a-javascript/dia-04-objetos-e-funcoes/script.js
@@ -5,17 +5,45 @@
 // let age = 34;
 // let medals = { golden: 2, silver: 3 };
 
-let player = {
-    name: 'Marta',
-    lastName: 'Silva',
-    age: 34,
-    medals: { golden: 2, silver: 3 }
-};
+// let player = {
+//     name: 'Marta',
+//     lastName: 'Silva',
+//     age: 34,
+//     medals: { golden: 2, silver: 3 }
+// };
 
-// console.log('A jogadora ' + player.name + ' ' + player.lastName + ' tem ' + player.age + ' anos de idade.')
+// // console.log('A jogadora ' + player.name + ' ' + player.lastName + ' tem ' + player.age + ' anos de idade.')
 
-player.bestInTheWorld = [2006 , 2007, 2008, 2009, 2010, 2018]
+// player.bestInTheWorld = [2006 , 2007, 2008, 2009, 2010, 2018]
 
-// console.log('A jogadora ' + player['name'] + ' ' + player['lastName'] + ' foi eleita a melhor do mundo por ' + player['bestInTheWorld'].length + ' vezes');
+// // console.log('A jogadora ' + player['name'] + ' ' + player['lastName'] + ' foi eleita a melhor do mundo por ' + player['bestInTheWorld'].length + ' vezes');
 
-console.log('A jogadora possui ' + player.medals.golden + ' medalhas de ouro e ' + player.medals.silver + ' medalhas de prata.');
+// console.log('A jogadora possui ' + player.medals.golden + ' medalhas de ouro e ' + player.medals.silver + ' medalhas de prata.');
+
+
+// function soma(a, b) {
+//     return a + b;
+// };
+// function subt(a, b) {
+//     return a - b;
+// };
+// function mult(a, b) {
+//     return a * b;
+// };
+// function divisao(a, b) {
+//     return a / b;
+// };
+// function modulo(a, b) {
+//     return a % b;
+// };
+
+// const num1 = 20;
+// const num2 = 30;
+
+// function maiorNumero(num1, num2) {
+//     if (num1 > num2) {
+//         return num1 + ' maior que ' + num2;
+//     } else {
+//         return num2 + ' maior que ' + num1;
+//     }
+// }
